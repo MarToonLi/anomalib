a7b65c1d63a829d8fc712d458e89419a80ded795
MarToonLi <1437623218@qq.com>
2024-11-19 19:13:03 +0800

    feat: 1. 3-5 - jing F1->0.92

--- notebooks/test.py
+++ notebooks/test.py
@@ -1,95 +1,114 @@
-from pathlib import Path
+import os
 import numpy as np
-from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint
-from matplotlib import pyplot as plt
 from PIL import Image
-from torch.utils.data import DataLoader
-import torch
+from pathlib import Path
+from torchvision.transforms.v2 import Resize
+from torchvision.transforms.v2.functional import to_pil_image
 
 from anomalib import TaskType
-from anomalib.data import MVTec, PredictDataset
-from anomalib.data.image.folder import Folder, FolderDataset
-
-from anomalib.engine import Engine
-from anomalib.models import Fastflow
-from anomalib.utils.post_processing import superimpose_anomaly_map
-
+import torch
 
 torch.set_float32_matmul_precision('medium')
 
-dataset_root = r"F:\\Projects\\anomalib\\datasets\\3-5"
+configs = {
+"dataset_root": r"F:\Projects\anomalib\notebooks\datasets\3-5",
+"outputs_path": r"F:\Projects\anomalib\notebooks\datasets\3-5\outputs",
+"model_name": "Patchcore",
+}
 
+dataset_root = configs["dataset_root"]
+print("dataset_root: {}".format(dataset_root))
 
-folder_datamodule = Folder(
-    name="3-5",
-    root=dataset_root,
-    normal_dir="normal",
-    abnormal_dir="abnormal",
-    task=TaskType.CLASSIFICATION,
-    image_size=(256, 256),
-    num_workers=2,
-    val_split_ratio=0.3,
-    normal_split_ratio = 0.2,
-    train_batch_size = 2,
-    eval_batch_size = 2,
-    seed = 1,
-)
-folder_datamodule.setup()
+normal_folder_path = os.path.join(configs["dataset_root"], "normal")
+abnormal_folder_path = os.path.join(configs["dataset_root"], "abnormal")
+
+normal_ouput_path = os.path.join(configs["outputs_path"], configs["model_name"] , "normal_outputs")
+abnormal_output_path = os.path.join(configs["outputs_path"], configs["model_name"] , "abnormal_outputs")
 
 
 
-i, data = next(enumerate(folder_datamodule.train_dataloader()))
-print("train",data.keys(), data["image"].shape)
+from lightning.pytorch.callbacks import EarlyStopping
+from anomalib.callbacks.checkpoint import ModelCheckpoint
+from anomalib.callbacks import GraphLogger
+from anomalib.loggers import AnomalibMLFlowLogger
+from torchvision.transforms.v2 import Resize, RandomHorizontalFlip, Compose, Normalize, ToDtype,RandomAffine,RandomPerspective, Grayscale, ToTensor, Transform
+from anomalib.data.image.folder import Folder, FolderDataset
 
-i, data = next(enumerate(folder_datamodule.val_dataloader()))
-print("val",data.keys(), data["image"].shape)
-
-i, data = next(enumerate(folder_datamodule.test_dataloader()))
-print("test",data.keys(), data["image"].shape)
-
-
-
-# Import the model and engine
-from anomalib.models import Patchcore
-from anomalib.engine import Engine
-
-# Create the model and engine
-model = Patchcore()
-engine = Engine(task="classification")
-
-# Train a Patchcore model on the given datamodule
-engine.train(datamodule=folder_datamodule, model=model)
+import warnings
+warnings.filterwarnings("ignore")
 
 
+class ExtractBChannel(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+        # 可以在这里设置一些参数（如果需要的话），例如大小，填充值等
+        pass
+    
+    # RGB (3, H, W) 的tensor类型
+    def forward(self, img):
+        # print(img.shape)
+        # b_channel = img[2:, :, :]  # 提取 B 通道（张量的第三个通道，索引为2）
+        # b_channel[b_channel < 100] = 0
+        return img
 
 
-# from anomalib.models import Padim
-# model = Padim(
-#     backbone="resnet18",
-#     layers=["layer1", "layer2", "layer3"],
-# )
+class MyCustomTransform(torch.nn.Module):
+    def forward(self, img):  # we assume inputs are always structured like this
+        print(".....................{}".format(img.shape))
+        return img
 
-
-
-# from anomalib.engine import Engine
-# from anomalib.utils.normalization import NormalizationMethod
-
-# engine = Engine(
-#     normalization=NormalizationMethod.MIN_MAX,
-#     threshold="F1AdaptiveThreshold",
-#     task=TaskType.CLASSIFICATION,
-#     image_metrics=["AUROC"],
-#     accelerator="auto",
-#     check_val_every_n_epoch=1,
-#     devices=1,
-#     max_epochs=3,
-#     num_sanity_val_steps=0,
-#     val_check_interval=1.0,
-# )
+if __name__ == "__main__":
 
 
 
-# engine.fit(datamodule=datamodule, model=model)
+    train_transform = Compose(
+        [
+            #Grayscale(),
+            MyCustomTransform(),
+            #ToTensor(),
+            #ToDtype(torch.uint8, scale=True),
+            Resize((256, 256)),
+            ##RandomAffine(degrees=(-5, 5), translate=(0.9, 0.9),scale=(0.95, 0.95), ),
+            #RandomPerspective(distortion_scale=0.1, p=0.3),
+            #ToDtype(torch.float32, scale=True),  # Normalize expects float input
+            #Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #Normalize(mean=[0.485], std=[0.229]),
+        ],
+    )
+
+    eval_transform = Compose(
+        [
+            #Grayscale(),
+            #ExtractBChannel(),
+            #ToTensor(),
+            #ToDtype(torch.uint8, scale=True),
+            Resize((256, 256)),
+            # RandomAffine(degrees=(-5, 5), translate=(0.9, 0.9),scale=(0.95, 0.95), ),
+            # RandomPerspective(distortion_scale=0.1, p=0.3),
+            #ToDtype(torch.float32, scale=True),  # Normalize expects float input
+            #Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+            #Normalize(mean=[0.485], std=[0.229]),
+        ],
+    )
 
 
-# engine.test(datamodule=datamodule, model=model)
+
+    folder_datamodule = Folder(
+        name="3-5",
+        root=dataset_root,
+        normal_dir="normal", abnormal_dir="abnormal",
+        task=TaskType.CLASSIFICATION,
+        num_workers=2,
+        # image_size=(256, 256),
+        train_batch_size = 2, eval_batch_size = 8,                     # 计算的时候会使用cuda，因此需要限制BS不适用默认值32；
+        train_transform=train_transform, eval_transform=eval_transform,
+    )
+
+    folder_datamodule.setup()            #! 进行数据集分割
+
+
+    temp = folder_datamodule.train_dataloader()
+
+    # Train images
+    i, data = next(enumerate(folder_datamodule.train_dataloader()))
+    print(data.keys(), data["image"].shape)
